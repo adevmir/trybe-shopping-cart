ea2fc0c73e5820e1d6976fe63a5d4fec39247080
adevmir <ademirvalonqar@gmail.com>
2022-03-28 19:07:16 -0300

    [Requisito 8]

--- helpers/getSavedCartItems.js
+++ helpers/getSavedCartItems.js
@@ -1,12 +1,8 @@
 const getSavedCartItems = () => {
   const items = localStorage.getItem('shopCart');
-  if (items === undefined || items === {}) {
-    console.log('0');
-  } else {
-    const cart = document.querySelector('.cart__items');
-    console.log(items);
-    cart.innerHTML = items;
-  }
+  const cart = document.querySelector('.cart__items');
+  console.log(items);
+  cart.innerHTML = items;
 };
 
 if (typeof module !== 'undefined') {
--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -6,22 +6,21 @@ const computadorSearch = require('../mocks/search');
 describe('1 - Teste a função fecthProducts', () => {
   // implemente seus testes aqui
   it('Teste se fetchProducts é uma função', () => {
-    const type = typeof fetchProducts;
-    expect(type).toBe('function');
+    expect(typeof fetchProducts).toBe('function');
   });
 
-  it('Execute a função fetchProducts com o argumento "computador" e teste se fetch foi chamada', async () => {
+  it('Execute a função fetchProducts com o argumento "computador" e teste se fetch foi chamada', () => {
     fetchProducts('computador');
     expect(fetch).toHaveBeenCalled();
   });
 
   it('Teste se, ao chamar a função fetchProducts com o argumento "computador", a função fetch utiliza o endpoint "https://api.mercadolibre.com/sites/MLB/search?q=computador"', async () => {
-    fetchProducts('computador');
+    await fetchProducts('computador');
     expect(fetch).toHaveBeenCalledWith('https://api.mercadolibre.com/sites/MLB/search?q=computador');
   });
 
   it('Teste se o retorno da função fetchProducts com o argumento "computador" é uma estrutura de dados igual ao objeto computadorSearch, que já está importado no arquivo', async () => {
-    const result  = fetchProducts('computador');
+    const result  = await fetchProducts('computador');
     expect(typeof result).toEqual(typeof computadorSearch);
   });
 
